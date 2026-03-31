d6fc81ae42b1e3961c47cec32206811ce6f618e0
Dzmitry Bukhavetski <d.bukhavetski@godeltech.com>
2026-03-31 15:24:32 +0200

    feat: set 3 workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: Boolean(process.env.CI),
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 3 : undefined,
   timeout: environment.defaultTimeout,
   reporter: [['html', { open: 'never' }], ['list']],
   use: {
